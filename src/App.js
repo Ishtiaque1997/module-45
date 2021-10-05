1e09f79b95662d4e333be6c4d856b2be45722581
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-05 22:45:29 +0600

    some-work-done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,39 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const number=555;
+  const singer={
+    name:'Dr. Mahfuz',
+    profession:'singer'
+  }
+  const singer2={
+    name:'eva rahman',
+    job:'kokila'
+  }
+  //using style as object
+  const singerStyle={
+    backgroundColor:'green',
+    padding:'10px'
+  }
+  //3 ays to use style
+  //1. externally 2. accessing as an object 3. internally with double curly braces
   return (
     <div className="App">
       <header className="App-header">
+      <h3>React starting</h3>
+      <p>Happy with react</p>
+      <div className='container'>
+        <h3>Ths is inside the container</h3>
+        <p>My number is {number}</p>
+        <p style={{color:'red'}}>Singer:{singer.name}</p>
+        <p>Job:{singer.profession}</p>
+        <p style={singerStyle}>Real singer: {singer2.name}</p>
+      </div>
+        
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h3>Edited</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
