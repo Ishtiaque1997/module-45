754fd44b7ae7f9024bb4b0b5bc7d8226e3022508
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-06 06:34:56 +0600

    component-going-on

--- src/App.js
+++ src/App.js
@@ -2,50 +2,50 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  const number=555;
-  const singer={
-    name:'Dr. Mahfuz',
-    profession:'singer'
-  }
-  const singer2={
-    name:'eva rahman',
-    job:'kokila'
-  }
+  // const number=555;
+  // const singer={
+  //   name:'Dr. Mahfuz',
+  //   profession:'singer'
+  // }
+  // const singer2={
+  //   name:'eva rahman',
+  //   job:'kokila'
+  // }
   //using style as object
-  const singerStyle={
-    backgroundColor:'green',
-    padding:'10px'
-  }
-  //3 ays to use style
+  // const singerStyle={
+  //   backgroundColor:'green',
+  //   padding:'10px'
+  // }
+  //3 ways to use style
   //1. externally 2. accessing as an object 3. internally with double curly braces
   return (
     <div className="App">
-      <header className="App-header">
-      <h3>React starting</h3>
-      <p>Happy with react</p>
-      <div className='container'>
-        <h3>Ths is inside the container</h3>
-        <p>My number is {number}</p>
-        <p style={{color:'red'}}>Singer:{singer.name}</p>
-        <p>Job:{singer.profession}</p>
-        <p style={singerStyle}>Real singer: {singer2.name}</p>
-      </div>
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person/>
+      <Person/> 
+       <Person/>
+      <Person/> 
+       <Person/>
+      <Person/>      
     </div>
   );
+}
+function Person(){
+  const person={
+    backgroundColor:'skyblue',
+    border:'3px solid red',
+    margin:'20px',
+    borderRadius:'20px'
+
+  }
+  return (
+  <div style={{backgroundColor:'skyblue',
+    border:'3px solid red',
+    margin:'20px',
+    borderRadius:'20px'}}>
+    <h1>Sakib Al Hasan</h1>
+    <h4>Profession:Cricketer</h4>
+  </div>
+  )
 }
 
 export default App;
